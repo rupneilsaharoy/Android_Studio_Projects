06c3e2d93584b18676fdb2ed90c338f62897c425
Rupneil Saha Roy <rupneilsaharoy@gmail.com>
2021-02-03 11:06:28 +0530

    Section 4

---
+++ Android_Section_4/FadingAnimations/app/src/main/java/com/example/fadinganimations/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.fadinganimations;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void fade(View view) {
+
+        Log.i("tag", "Imageview tapped");
+
+        ImageView bartImageView = (ImageView) findViewById(R.id.bartImageView);
+
+        ImageView homerImageView = (ImageView) findViewById(R.id.homerImageView);
+
+        bartImageView.animate().scaleX(0.5f).scaleY(0.5f).setDuration(1000);
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView bartImageView = (ImageView) findViewById(R.id.bartImageView);
+
+        bartImageView.setX(-1000);
+
+        bartImageView.animate().translationXBy(1000).rotation(3600).setDuration(2000);
+    }
+}
