ddfc9edfafc79437fb92385d479f2586986501de
Rupneil Saha Roy <rupneilsaharoy@gmail.com>
2021-02-08 13:33:32 +0530

    Maps

---
+++ Maps_and_Geolocation/MultipleActivityDemo/app/src/main/java/com/example/multipleactivitydemo/SecondActivity.java
@@ -0,0 +1,35 @@
+package com.example.multipleactivitydemo;
+
+/* Cannot create an empty activity on Mac. Keeps on freezing. */
+
+/* https://stackoverflow.com/questions/64952961/android-studio-4-1-1-macos-bigsur-freezing-problem-in-fullscreen */
+
+/* Solution number 17 */
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    public void goBack(View view) {
+        // Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        // startActivity(intent);
+
+        finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+
+        Toast.makeText(this,name, Toast.LENGTH_SHORT).show();
+    }
+}
